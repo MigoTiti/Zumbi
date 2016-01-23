257b9293fb3ec63c7ba3b930448909436257764d
Lucas Rodrigues <lucasrodriguesgui@hotmail.com>
2016-01-23 02:41:38 -0300

    Criadas duas subclasses: Mapa1 e Mapa2, que herdam o mesmo tamanho de matriz, mas são duas "fases" diferentes. Atualização do "main.cpp" para implementar a segunda fase e mudança de tipo de atributo em "Mapa.h" de "private" para "protected", assim permitindo às subclasses usarem o mesmo projeto de matriz e o mesmo tamanho.

--- Mapa.h
+++ Mapa.h
@@ -2,14 +2,27 @@
 class Mapa{
 	public:
 		Mapa(int);
-		void iniciarMapa(Mapa*, int);
-        void exibirMapa(Mapa*);
-        void andarMapa(Mapa*, char, Zumbi*);
-        void verificarMapa(Mapa*, char, int, int, int*, int*, Zumbi*);
-        void procurarMapa(Mapa*, int*, int*);
-        void setSize(int);
-        void setMap(int, int);
-    private:
+    protected:
     	int tam;
 		char mapa[30][30];	
+};
+
+class Mapa1 : public Mapa{
+	public:
+		Mapa1(int);
+		void iniciarMapa(Mapa1*, int);
+		void exibirMapa(Mapa1*);
+        void andarMapa(Mapa1*, char, Zumbi*, int*);
+        void verificarMapa(Mapa1*, char, int, int, int*, int*, Zumbi*, int*);
+        void procurarMapa(Mapa1*, int*, int*);
+};
+
+class Mapa2 : public Mapa{
+	public:
+		Mapa2(int);
+		void iniciarMapa(Mapa2*, int);
+		void exibirMapa(Mapa2*);
+        void andarMapa(Mapa2*, char, Zumbi*);
+        void verificarMapa(Mapa2*, char, int, int, int*, int*, Zumbi*);
+        void procurarMapa(Mapa2*, int*, int*);
 };
--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 void escolha2(int opt);
-void escolha(int op, Zumbi z1, Mapa *m1);
+void escolha(int op, Zumbi z1, Mapa1 *m1, Mapa2 *m2);
 int menu();
 
 HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -23,18 +23,21 @@ int main(){
 	z1.setSpeed(1);
 	z1.setHealth(5000);
 	
-    Mapa m1(30);
+    Mapa1 m1(30);
 	m1.iniciarMapa(&m1,8);
+	
+	Mapa2 m2(30);
+	m2.iniciarMapa(&m2,8);
 	
 	int op;
 	do{
 	op=menu();
-	escolha(op,z1,&m1);
+	escolha(op,z1,&m1,&m2);
     }while(op!=3);
 }	
 
-void escolha(int op, Zumbi z1, Mapa *m1){
-	int opt, i, j;
+void escolha(int op, Zumbi z1, Mapa1 *m1, Mapa2 *m2){
+	int opt, i, j, c=0;
 	char d;
 	switch(op){
 		case 1:
@@ -49,13 +52,21 @@ void escolha(int op, Zumbi z1, Mapa *m1){
 			m1->exibirMapa(m1);
 			cout << "\nUse o numpad para andar: ";
 			d=getche();
-			m1->andarMapa(m1,d,&z1);
+			m1->andarMapa(m1,d,&z1,&c);
 			break;
 		case 3:
 			exit(0);
 			break;
 		default:
 			cout << "Comando invalido";
+	}
+	if (c==8){
+	system("cls");
+	cout << "Voce chegou ao nivel 2.";
+	do{
+	    cin.get();
+	}while(cin.get()!='\n');
+	m2->andarMapa(m2,d,&z1);
 	}
 }
 
